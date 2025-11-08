99f78266e03e7f0611bdebe99f32c8febef033ac
Nawaf <nawafotaibi.ot@gmail.com>
2025-11-08 10:29:43 +0300

    Create [Entity].java for database entity representation
    Added a POJO class for representing database entities.

---
+++ src/model/Entity.java
@@ -0,0 +1,11 @@
+/**
+ * POJO (Plain Old Java Object) representing database entity.
+ * 
+ * (e.g. customer.java, order.java, ...)
+ *
+ * Contains:
+ * - Private fields matching database columns
+ * - Getters and setters
+ * - Constructor(s)
+ * - toString() for debugging
+ */
