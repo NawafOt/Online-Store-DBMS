710e712449692b3e4865bd167b6853d327f7c52b
Nawaf <nawafotaibi.ot@gmail.com>
2025-11-08 10:26:16 +0300

    Create BaseDAO class for generic CRUD operations
    This class serves as a base for all DAO implementations, providing common CRUD operations.

---
+++ src/dao/BaseDAO.java
@@ -0,0 +1,11 @@
+/**
+ * Abstract base class for all DAOs.
+ * 
+ * Provides generic CRUD operations:
+ * - insert(Entity entity)
+ * - update(Entity entity)
+ * - delete(int id)
+ * - findById(int id)
+ * - findAll()
+ * - search(String criteria)
+ */
