b2de457071518937ed13ff6ce6b804ae79976800
Shassius <445103486@student.ksu.edu.sa>
2025-11-19 20:07:48 +0300

    made css get applied in main

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,12 @@ import java.util.Objects;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ui/LoginPage.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ui/login.fxml")));
         Scene scene = new Scene(root, 900, 600);
+
+        String css = Objects.requireNonNull(getClass().getResource("/styles/app.css")).toExternalForm();
+        scene.getStylesheets().add(css);
+
         primaryStage.setTitle("E-Commerce Shop");
         primaryStage.setScene(scene);
         primaryStage.show();
