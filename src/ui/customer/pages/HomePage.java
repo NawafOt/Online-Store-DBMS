8dc22dd55c09ccbcea606852ddffbd002cee799c
Shassius <445103486@student.ksu.edu.sa>
2025-11-20 03:09:24 +0300

    added very basic functionality and edited somethings

--- src/ui/customer/pages/HomePage.java
+++ src/ui/customer/pages/HomePage.java
@@ -1,4 +1,37 @@
 package ui.customer.pages;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import ui.PageManager;
+
+import java.util.Objects;
+
 public class HomePage {
+
+    @FXML
+    private ImageView iconLogoutView;
+    @FXML
+    private ImageView iconCartView;
+
+    @FXML
+    public void initialize() {
+        try {
+            Image iconLogout = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/icons/log-out.png")));
+            iconLogoutView.setImage(iconLogout);
+            Image iconCart = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/icons/cart.png")));
+            iconCartView.setImage(iconCart);
+        } catch (Exception e) {
+            System.err.println("Failed to load icon: " + e.getMessage());
+        }
+    }
+
+    @FXML
+    public void handleProfile() {
+        PageManager.loadPage("customer/pages/profile.fxml");
+    }
+
+    public void handleLogout() {
+        PageManager.loadPage("login.fxml");
+    }
 }
--- src/ui/customer/pages/RegisterPage.java
+++ src/ui/customer/pages/RegisterPage.java
@@ -1,4 +1,23 @@
 package ui.customer.pages;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
 public class RegisterPage {
+
+    @FXML
+    private ImageView iconReturnView;
+
+    @FXML
+    public void initialize() {
+        try {
+            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/icons/undoWhite.png")));
+            iconReturnView.setImage(icon);
+        } catch (Exception e) {
+            System.err.println("Failed to load icon: " + e.getMessage());
+        }
+    }
 }
