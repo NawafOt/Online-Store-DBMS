b9987baea8151b3c3d89b70034b8f948768416d6
Shassius <445103486@student.ksu.edu.sa>
2025-11-23 04:58:54 +0300

    added class for easy Card Action generation

---
+++ src/ui/customer/pages/CardAction.java
@@ -0,0 +1,11 @@
+package ui.customer.pages;
+
+public class CardAction {
+    public final String text;
+    public final Runnable action;
+
+    public CardAction(String text, Runnable action) {
+        this.text = text;
+        this.action = action;
+    }
+}
