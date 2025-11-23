2f5542f8f1b4f5c72093afe100c1668933332a1e
Shassius <445103486@student.ksu.edu.sa>
2025-11-23 05:04:00 +0300

    extracted loading logic and added return logic

--- src/ui/customer/pages/RegisterPage.java
+++ src/ui/customer/pages/RegisterPage.java
@@ -3,6 +3,8 @@ package ui.customer.pages;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import model.Session;
+import ui.PageManager;
 
 import java.util.Objects;
 
@@ -13,11 +15,20 @@ public class RegisterPage {
 
     @FXML
     public void initialize() {
+        loadImages();
+        Session.getInstance().logout();
+    }
+
+    private void loadImages() {
         try {
             Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/icons/undoWhite.png")));
             iconReturnView.setImage(icon);
         } catch (Exception e) {
             System.err.println("Failed to load icon: " + e.getMessage());
         }
+    }
+
+    public void handleReturn() {
+        PageManager.loadPage("login.fxml");
     }
 }
